03497a8087f1047546bbdf20edfb067a3bf1462d
Roman <Roman@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-05-22 14:45:58 +0000

    Informer style: renaming files.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/styles@1180 4dc816b1-62af-4023-b237-cae3452c1eb1

--- Informer/Informer.php
+++ Informer/Informer.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/informer.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/informer_cs.css" />
-<!--[if lte IE 6]><link rel="stylesheet" type="text/css" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/informer_ie6.css" /><![endif]-->
-<!--[if IE 7]><link rel="stylesheet" type="text/css" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/informer_ie7.css" /><![endif]-->
-<!--[if lt IE 7]><script src="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/informer_ie6.js" type="text/javascript"></script><![endif]-->
+<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Informer.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Informer_cs.css" />
+<!--[if lte IE 6]><link rel="stylesheet" type="text/css" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Informer_ie6.css" /><![endif]-->
+<!--[if IE 7]><link rel="stylesheet" type="text/css" href="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Informer_ie7.css" /><![endif]-->
+<!--[if lt IE 7]><script src="<?php echo $base_url.'/style/'.$forum_user['style'] ?>/Informer_ie6.js" type="text/javascript"></script><![endif]-->
